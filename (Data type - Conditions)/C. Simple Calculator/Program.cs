e8218372db88464ae93c427806dc36aa4b5cf50b
Karamreda <148919995+Karamreda@users.noreply.github.com>
2026-05-07 00:38:58 +0300

    Add files via upload

---
+++ (Data type - Conditions)/C. Simple Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        // input
+        string[] x = Console.ReadLine().Split();
+
+        long a = long.Parse(x[0]);
+        long b = long.Parse(x[1]);
+        // output
+        Console.WriteLine(a + " + " + b + " = " + (a + b));
+        Console.WriteLine(a + " * " + b + " = " + (a * b));
+        Console.WriteLine(a + " - " + b + " = " + (a - b));
+    }
+}
