096d17be7a72c2638e23e71f81b2b4b52b444c2f
Karamreda <148919995+Karamreda@users.noreply.github.com>
2026-05-06 17:27:04 +0300

    Add files via upload

---
+++ (Data type - Conditions)/B. Basic Data Types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Data Types
+        int i;
+        long ll;
+        char c;
+        float f;
+        double d;
+        // Read input 
+        string[] x = Console.ReadLine().Split();
+
+        i = int.Parse(x[0]);
+        ll = long.Parse(x[1]);
+        c = char.Parse(x[2]);
+        f = float.Parse(x[3]);
+        d = double.Parse(x[4]);
+
+        // Print 
+        Console.WriteLine(i);
+        Console.WriteLine(ll);
+        Console.WriteLine(c);
+        Console.WriteLine(f);
+        Console.WriteLine(d);
+        // ممكن الحل يبقي في سطر بسيط زي دا 
+        //Console.WriteLine(Console.ReadLine().Replace(" ", "\n"));
+
+    }
+}
+
