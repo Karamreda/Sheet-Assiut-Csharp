072482d3e6a8bcaa181b7ce28bbf813045e25d93
Karamreda <148919995+Karamreda@users.noreply.github.com>
2026-05-07 06:19:10 +0300

    Add files via upload

---
+++ (Data type - Conditions)/E. Area of a Circle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the radius 
+        double R = double.Parse(Console.ReadLine());
+        // Calculate the area of the circle
+        double area = 3.141592653 * R * R;
+        // Print the area with 9 decimal places
+        Console.WriteLine(area.ToString("F9"));
+    }
+}
