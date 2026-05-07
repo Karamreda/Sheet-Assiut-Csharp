70acbd8087f1462bdd44217e615bd4e1f87f4180
Karamreda <148919995+Karamreda@users.noreply.github.com>
2026-05-07 06:18:11 +0300

    Add files via upload

---
+++ (Data type - Conditions)/D. Difference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        // Variables
+        long A , B , C , D;
+
+
+        //Read Input
+        string[] input = Console.ReadLine().Split(' ');
+        A = long.Parse(input[0]);
+        B = long.Parse(input[1]);
+        C = long.Parse(input[2]);
+        D = long.Parse(input[3]);
+
+        // Calculate and Print Output
+        Console.WriteLine($"Difference = {((A * B) - (C * D))}");
+
+    }
+}
