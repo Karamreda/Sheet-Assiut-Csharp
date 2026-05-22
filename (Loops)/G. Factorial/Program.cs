aef527e4ed2a1d86cc7c8b40fac3446355f8caf4
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-22 03:20:24 +0300

    Add files via upload

---
+++ (Loops)/G. Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Read the number of test cases
+        int T = int.Parse(Console.ReadLine());
+        
+        while (T > 0)
+        {
+            // Read the integer N for which we need to calculate the factorial
+            int N = int.Parse(Console.ReadLine());
+          
+            
+            // Calculate the factorial of N
+            long factorial = 1;
+
+            for (int i = 1; i <= N; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+
+            T--;
+        }
+    }
+}
---
+++ (Loops)/H. One Prime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read input
+        int x = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+
+
+        for (int i = 2; x / i >= i; i++)
+        //for (int i = 2; i * i <= x; i++)
+
+        {
+            if (x % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            Console.WriteLine("YES");
+        else
+            Console.WriteLine("NO");
+    }
+}
