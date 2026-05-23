bdccfd195e66d3d7d1c37674c840bf77427dfb0e
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-23 03:56:43 +0300

    Add files via upload

---
+++ (Loops)/L. GCD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read two integers 
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        // Calculate the GCD using the  >>>> Euclidean algo
+        while (b != 0)
+        {
+
+            int temp = b; // temporary variable
+            b = a % b; 
+            a = temp;
+        }
+
+        Console.WriteLine(a);
+    }
+}
