cf654b843903d7584b8ec715eb26162a0811a6ea
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-23 04:19:32 +0300

    Modify GCD program to read integers from a single line

--- (Loops)/L. GCD/Program.cs
+++ (Loops)/L. GCD/Program.cs
@@ -5,9 +5,10 @@ class Program
     static void Main()
     {
         // Read two integers 
+        string[] input = Console.ReadLine().Split();
 
-        int a = int.Parse(Console.ReadLine());
-        int b = int.Parse(Console.ReadLine());
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
 
         // Calculate the GCD using the  >>>> Euclidean algo
         while (b != 0)
