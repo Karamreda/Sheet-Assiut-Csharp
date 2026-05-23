b4f0248b6d21d6660ad7a07758409558350a7efa
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-23 05:53:40 +0300

    Add files via upload

---
+++ (Loops)/N. Numbers Histogram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read input
+        char C = char.Parse(Console.ReadLine());
+        int N = int.Parse(Console.ReadLine());
+        string[] arr = Console.ReadLine().Split();
+
+
+        // Loop to Read Numbers 
+        for (int i = 0; i < N; i++)
+        {
+            int x = int.Parse(arr[i]);
+
+            // Loop to print char
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write(C);
+            }
+            //New line
+            Console.WriteLine();
+        }
+    }
+}
