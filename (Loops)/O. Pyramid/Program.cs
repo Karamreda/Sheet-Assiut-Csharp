7895f2a9553d401a152f36d6af873dfd09d38646
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-24 18:10:10 +0300

    Add files via upload

---
+++ (Loops)/O. Pyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read input
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            // Print Char (*) i time
+            Console.WriteLine(new string('*', i));
+        }
+
+        // Anther way with Nested loop
+        /*
+        for (int i = 1; i <= N; i++)
+        {
+
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+        }*/
+    }
+}
