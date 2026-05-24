e3b0d50f0dd77da52b5aaa0033b911f872b846df
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-05-24 18:11:47 +0300

    Add files via upload

---
+++ (Loops)/Q. Digits/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Read the number of test cases
+        int T = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 0; i < T; i++)
+        {
+
+            int N = int.Parse(Console.ReadLine()); // Read the integer 
+
+            
+            if (N == 0)
+            {
+                Console.WriteLine(0);
+            }
+
+
+
+            else
+            {
+                while (N > 0)
+                {
+                    Console.Write(N % 10 + " "); // Print the last digit and space
+                    N /= 10; // Remove the last digit
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
