46616d2613b74b0f80cd08bb54f9cb27915856c6
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-07-04 18:42:27 +0300

    Add files via upload

---
+++ (Loops)/V. PUM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Read the number of test cases
+        int N = int.Parse(Console.ReadLine());
+
+        int num = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"{num} {num + 1} {num + 2} PUM");
+            num += 4;
+        }
+    }
+}
