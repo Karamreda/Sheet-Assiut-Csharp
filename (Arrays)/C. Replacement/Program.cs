418ff2e61de72c8892605ac7403c73a87897d0b4
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-07-05 13:52:18 +0300

    Add files via upload

---
+++ (Arrays)/C. Replacement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int N = int.Parse(Console.ReadLine());
+
+        string[] input = Console.ReadLine().Split();
+
+        foreach (var item in input)
+        {
+            int num = int.Parse(item);
+
+            if (num > 0)
+                Console.Write("1 ");
+            else if (num < 0)
+                Console.Write("2 ");
+            else
+                Console.Write("0 ");
+        }
+    }
+}
---
+++ (Arrays)/D. Positions in array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Read the number of elements in the array
+        int N = int.Parse(Console.ReadLine());
+
+        // Read the array 
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        
+        for (int i = 0; i < N; i++)
+        {
+            int num = input[i];
+
+            if (num <= 10)
+            {
+                Console.WriteLine($"A[{i}] = {num}");
+            }
+        }
+
+    }
+}
