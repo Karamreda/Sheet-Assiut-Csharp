4196f09b45d8d403384f59eba55d9e8cd13fab63
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-07-06 06:42:12 +0300

    Add files via upload

---
+++ (Loops)/Z. Three Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read input 
+        string[] input = Console.ReadLine().Split();
+
+        int k = int.Parse(input[0]);
+        int s = int.Parse(input[1]);
+
+        int count = 0;
+
+        for (int x = 0; x <= k; x++)
+        {
+            for (int y = 0; y <= k; y++)
+            {
+
+                int z = s - x - y;
+
+                if (z >= 0 && z <= k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
