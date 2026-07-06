801c171fa00fee174f6c948bce44080d444fbee3
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-07-06 06:40:18 +0300

    Add files via upload

---
+++ (Loops)/W. Shape3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        // loop to print the upper 
+        for (int i = 1; i <= N; i++)
+        {
+            Console.Write(new string(' ', N - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+        // loop to print the lower 
+        for (int i = N; i >= 1; i--)
+        {
+            Console.Write(new string(' ', N - i));
+            Console.WriteLine(new string('*', 2 * i - 1));
+        }
+    }
+}
