aa5e0ce368131a9b3adc62b767fe0f7c1e3aa8ba
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-07-06 06:41:13 +0300

    Add files via upload

---
+++ (Loops)/X. Convert To Decimal 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the number of test cases
+        int t = int.Parse(Console.ReadLine());
+
+        // Process each test case
+        while (t-- > 0)
+        {
+            // Read the input number
+            long n = long.Parse(Console.ReadLine());
+            
+            int ones = 0;
+
+            while (n > 0)
+            {
+                if ((n & 1) == 1)
+                    ones++;
+
+                n >>= 1;
+            }
+
+            long result = (1L << ones) - 1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ (Loops)/Y. Easy Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // read the number of terms to print
+        int n = int.Parse(Console.ReadLine());
+
+        
+        long a = 0, b = 1;
+
+        // print the Fibonacci series up to n terms
+        for (int i = 1; i <= n; i++)
+        {
+            
+            if (i == 1) // first term is 0
+            {
+                Console.Write("0 ");
+            }
+            else if (i == 2) // second term is 1
+            {
+                Console.Write("1 ");
+            }
+            else 
+            {
+                long c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
