c2891b519b0e38d47771d5387deff941c00898ab
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-09-07 09:53:38 +0300

    Add files via upload

---
+++ Recursion/E. Base Converssion/E. Base Converssion/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace E.Base_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine()); // test cases
+
+            while (T-- > 0) 
+            {
+                int n = int.Parse(Console.ReadLine()); // number to convert
+
+                PrintBinary(n); // print binary representation of n
+
+                Console.WriteLine(); // print a new line 
+            }
+        }
+
+        static void PrintBinary(int n)
+        {
+            if (n == 0) // base case
+                return;
+
+            PrintBinary(n / 2); // recursive call
+
+            Console.Write(n % 2); // print the remainder
+        }
+    }
+}
