079d67d4fb1f90b7f6a455b3d2d361256314a9ff
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-09-07 05:06:38 +0300

    Add files via upload

---
+++ (Recursion)/Q. 3n + 1 sequence/Q. 3n + 1 sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Q._3n___1_sequence
+{
+    internal class Program
+    {
+        static int Q3n1(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            // Even
+            if (n % 2 == 0)
+                return 1 + Q3n1(n / 2);
+
+            // Odd
+            return 1 + Q3n1(3 * n + 1);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Q3n1(n));
+        }
+
+    }
+}
