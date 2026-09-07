4e435c237702d20ee77ebfa315fa2b88ede367c1
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-09-07 07:02:28 +0300

    Add files via upload

---
+++ (Recursion)/V. Creating Expression1/V. Creating Expression1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace V._Creating_Expression1
+{
+        internal class Program
+        {
+            static bool CanMake(int[] arr, int index, long sum, long x)
+            {
+                // Base case: reached the end of the array
+                if (index == arr.Length)
+                    return sum == x;
+
+                // Try adding the current number
+                bool add = CanMake(
+                    arr,
+                    index + 1,
+                    sum + arr[index],
+                    x
+                );
+
+                // If a solution is found, return true
+                if (add)
+                    return true;
+
+                // Try subtracting the current number
+                bool subtract = CanMake(
+                    arr,
+                    index + 1,
+                    sum - arr[index],
+                    x
+                );
+
+                return subtract;
+            }
+
+            static void Main(string[] args)
+            {
+                string[] firstLine = Console.ReadLine().Split();
+
+                int n = int.Parse(firstLine[0]);
+                long x = long.Parse(firstLine[1]);
+
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+                // The first number has no sign before it
+                bool result = CanMake(arr, 1, arr[0], x);
+
+                Console.WriteLine(result ? "YES" : "NO");
+            }
+        }
+    }
