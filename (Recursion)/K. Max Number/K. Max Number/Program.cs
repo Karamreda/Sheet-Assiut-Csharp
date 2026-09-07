eac74491b44f04d664acc92e98716a299b7e1b66
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-09-07 03:11:50 +0300

    Add files via upload

---
+++ (Recursion)/K. Max Number/K. Max Number/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace K._Max_Number
+{
+    internal class Program
+    {
+        static long MaxNumber(long[] arr, int index) 
+        {
+            // Base Case
+            if (index == arr.Length - 1) 
+                return arr[index];
+
+            // Recursive Call
+            long max = MaxNumber(arr, index + 1);
+
+            return Math.Max(arr[index], max); 
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // size of the array
+
+            long[] arr = Array.ConvertAll(Console.ReadLine().Split(),long.Parse);
+
+            Console.WriteLine(MaxNumber(arr, 0));
+        }
+    }
+}
