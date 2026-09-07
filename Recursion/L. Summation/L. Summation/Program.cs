7aab4e59efde2a1f0b0c9e263af938e9fbac6d9d
Karam Reda Zidan <148919995+Karamreda@users.noreply.github.com>
2026-09-07 09:56:17 +0300

    Add files via upload

---
+++ Recursion/L. Summation/L. Summation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L._Summation
+{
+    internal class Program
+    {
+        static int Sum(int[] arr, int index)
+        {
+            // Base Case
+            if (index == arr.Length) 
+                return 0;
+
+            // Recursive Case
+            return arr[index] + Sum(arr, index + 1);
+        }
+
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());  // size of the array
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); // array
+
+            Console.WriteLine(Sum(arr, 0));
+        }
+    }
+}
---
+++ Recursion/M. Suffix Sum/M. Suffix Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace M._Suffix_Sum
+{
+    internal class Program
+    {
+        static long Sum(int[] arr, int index)
+        {
+            // Base Case
+            if (index == arr.Length)
+                return 0;
+
+            // Recursive Case
+            return arr[index] + Sum(arr, index + 1);
+        }
+
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int n = int.Parse(input[0]); // Size of the array
+            int m = int.Parse(input[1]); // Number of elements to sum from the end
+
+            int[] arr = Array.ConvertAll( Console.ReadLine().Split(), int.Parse);
+
+            int start = n - m; // Calculate the starting index for the suffix sum
+
+            Console.WriteLine(Sum(arr, start));
+        }
+    }
+}
